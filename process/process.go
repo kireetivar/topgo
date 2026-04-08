35d5e4345d1a049756ffbdd35af40f0414ef7ad6
kireetivar <kireeti3901@gmail.com>
2026-04-08 10:10:58 +0530

    read proces info

--- process/process.go
+++ process/process.go
@@ -1,8 +1,10 @@
 package process
 
 import (
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Process struct {
@@ -15,16 +17,41 @@ type Process struct {
 func GetProcessList() ([]Process, error) {
 	items, err := os.ReadDir("/proc")
 	if err != nil {
-		return []Process{}, err
+		return nil, err
 	}
 	var processes []Process
 	for _, item := range items {
 		if item.IsDir() {
 			pid, err := strconv.ParseInt(item.Name(), 10, 64)
 			if err == nil {
-				processes = append(processes, Process{PID: pid})
+				proc, err := readProcessInfo(pid)
+				if err == nil {
+					processes = append(processes, proc)
+				}
 			}
 		}
 	}
 	return processes, nil
+}
+
+func readProcessInfo(pid int64) (Process, error) {
+	stat, err := os.ReadFile(fmt.Sprintf("/proc/%d/stat", pid))
+	if err != nil {
+		return Process{}, err
+	}
+	fields := strings.Fields(string(stat))
+	var name string
+	var mem float64
+	for i, field := range fields {
+		if i == 1 {
+			name = strings.Trim(field, "()")
+		}
+		if i == 23 {
+			parsed, err := strconv.ParseFloat(field, 64)
+			if err == nil {
+				mem = parsed
+			}
+		}
+	}
+	return Process{PID: pid, Name: name, Mem: mem / 1024}, nil
 }
